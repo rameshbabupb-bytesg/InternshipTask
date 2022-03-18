1ec99aaeb9baf7e594341ced84908f58eceb04a2
Roshini290 <100557461+Roshini290@users.noreply.github.com>
2022-03-18 13:51:48 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+//Return the First Element in an Array 
+
+//Function Declaration
+function arr(n) {
+  // Storing the first item in a variable First
+    let First = n[0]; 
+    return First;  
+}
+
+//Array declaration and Function Calling
+let Result = arr([ "Hello JS", 2.5, 3, 4, 5 ]);
+
+// Printing the output to screen
+console.log(" First element in the array = ", Result);
